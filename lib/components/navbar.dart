ee3c53d77ed0f51261bff06962838271f0525f96
Zalz <arszalzdarker@gmail.com>
2025-10-06 11:33:45 +0700

    first commit

---
+++ lib/components/navbar.dart
@@ -0,0 +1,51 @@
+import 'package:belajar_flutter/pages/search.dart';
+import 'package:belajar_flutter/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int _selectedIndex = 0;
+
+  void _changeIndex(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navbar Example'),
+        backgroundColor: Colors.blue,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomePage(),
+          SearchPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _changeIndex,
+        selectedItemColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
